e7d4d0d2a9e085176ae070a0ba2868d4a9c4bf0a
manibharadwajcr@gmail.com <manibharadwajcr@gmail.com>
2026-04-11 12:34:53 +0530

    changes amde for 301

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,56 @@ const nextConfig: NextConfig = {
         destination: '/visa/:id-visa',
         permanent: true,
       },
+      {
+        source: '/about-us',
+        destination: '/about',
+        permanent: true,
+      },
+      {
+        source: '/contact/ahmedabad',
+        destination: '/contact/ahmedabad/visa-agents-in-ahmedabad',
+        permanent: true,
+      },
+      {
+        source: '/visa-checklist-for-first-time-travelers-2026-complete-step-by-step-guide',
+        destination: '/blog/visa-checklist-for-first-time-travelers-2026-complete-step-by-step-guide',
+        permanent: true,
+      },
+      {
+        source: '/name-mismatch-between-passport-tickets-will-you-be-stopped',
+        destination: '/blog/name-mismatch-between-passport-tickets-will-you-be-stopped',
+        permanent: true,
+      },
+      {
+        source: '/how-to-check-ecr-ecnr-status-on-passport-online',
+        destination: '/blog/how-to-check-ecr-ecnr-status-on-passport-online',
+        permanent: true,
+      },
+      {
+        source: '/visa/canada-visa',
+        destination: '/visa/canada-visa-consultant',
+        permanent: true,
+      },
+      {
+        source: '/contact/delhi',
+        destination: '/contact/delhi/visa-agents-in-connaught-place',
+        permanent: true,
+      },
+      {
+        source: '/contact/jaipur',
+        destination: '/contact/jaipur/visa-agents-in-jaipur',
+        permanent: true,
+      },
+      {
+        source: '/get-a-quote',
+        destination: '/reachout',
+        permanent: true,
+      },
+      {
+        source: '/contact/navi-mumbai',
+        destination: '/contact/mumbai/visa-agents-in-goregaon-east',
+        permanent: true,
+      },
     ];
   },
 };
